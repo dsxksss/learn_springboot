499f70df198b6dcee61d2afc9800b6af2df59569
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-12-29 18:22:36 +0800

    coffeeMapper添加剩余可能出现的方法、添加各方法单元测试((待优化)

--- src/main/java/com/example/learn_springboot/mappers/CoffeeMapper.java
+++ src/main/java/com/example/learn_springboot/mappers/CoffeeMapper.java
@@ -3,20 +3,15 @@ package com.example.learn_springboot.mappers;
 import com.example.learn_springboot.entitys.Coffee;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 // import org.apache.ibatis.annotations.Results;
 // import org.apache.ibatis.annotations.Result;
 
-// Integer id;// 产品ID
-// String name;// 产品名称
-// double price;// 产品价格
-// Integer quantity;// 售卖数量
-// String image;// 产品图片
-// String description;// 产品描述
-// long createTime;// 创建时间
-
 /*
- * 
  * @Select 是查询类的注解，所有的查询均使用这个
  * @Result 修饰返回的结果集，关联实体类属性和数据库字段一一对应，
  *    如果实体类属性和数据库属性名保持一致，就不需要这个属性来修饰。
@@ -25,12 +20,33 @@ import org.apache.ibatis.annotations.Select;
  * @delete 负责删除
  */
 
-public interface CoffeeMapper {
+/*
+ * Integer id;// 产品ID
+ * String name;// 产品名称
+ * double price;// 产品价格
+ * Integer quantity;// 售卖数量
+ * String image;// 产品图片
+ * String description;// 产品描述
+ * long createTime;// 创建时间
+ */
 
+public interface CoffeeMapper {
   // TODO: 待实现剩下功能
   @Select("SELECT * FROM coffees")
   List<Coffee> getAll();
 
-  @Select("SELECT * FROM coffees WHERE _name = #{coffeeName}")
-  Coffee getCoffeeBayName(String coffeeName);
+  @Select("SELECT * FROM coffees WHERE id = #{id}")
+  Coffee getOne(Integer id);
+
+  @Insert(
+    "INSERT INTO coffees(id,name,price,quantity,image,description,createTime) VALUES(#{id},#{name},#{price},#{quantity},#{image},#{description},#{createTime}) "
+  )
+  void insert(Coffee coffee);
+  
+  @Update("UPDATE coffees SET name=#{name},description=#{description} WHERE id=#{id}")
+  void update(Coffee coffee);
+  
+  @Delete("DELETE FROM coffees WHERE id=#{id}")
+  void delete(Integer id);
+  
 }
--- src/test/java/com/example/learn_springboot/LearnSpringbootApplicationTests.java
+++ src/test/java/com/example/learn_springboot/LearnSpringbootApplicationTests.java
@@ -1,13 +1,50 @@
 package com.example.learn_springboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
+import com.example.learn_springboot.entitys.Coffee;
+import com.example.learn_springboot.mappers.CoffeeMapper;
+
+// ! 注意 spring test 是同步执行的
 @SpringBootTest
 class LearnSpringbootApplicationTests {
 
+	@Autowired
+	private CoffeeMapper coffeeMapper;
+
 	@Test
-	void contextLoads() {
+	void testInsert() throws Exception {
+		coffeeMapper.insert(new Coffee(1, "Apple iPad Pro", 899.99, 100, "images/ipadpro.jpg", "The new Apple iPad Pro with 12.9-inch Retina display", 1587882545));
+		coffeeMapper.insert(new Coffee(2, "Samsung Galaxy S20", 699.99, 200, "images/s20.jpg", "The new Samsung Galaxy S20 with 6.2-inch Infinity-O Display", 1588067345));
+		coffeeMapper.insert(new Coffee(3, "Xiaomi Mi Note 10", 399.99, 300, "images/minote10.jpg", "The new Xiaomi Mi Note 10 with 6.47-inch AMOLED display", 1588251745));
+		Assert.isTrue(3==coffeeMapper.getAll().size(), "testInsert error !");
+	}
+	
+	@Test
+	void testQuery() throws Exception {
+		System.out.println(coffeeMapper.getAll());
+		Assert.isTrue(coffeeMapper.getAll().size() >= 0, "testQuery error !");
 	}
 
+	@Test
+	void testUpdate() throws Exception {
+		Coffee coffee = coffeeMapper.getOne(3);
+		System.out.println(coffee);
+		coffee.setName("test test test !!!");
+		coffeeMapper.update(coffee);
+		Assert.isTrue(coffee.getName().equals( coffeeMapper.getOne(3).getName()), "testUpdate error !");
+	}
+	
+	@Test
+	void testDelete() throws Exception {
+		coffeeMapper.delete(1);
+		coffeeMapper.delete(2);
+		coffeeMapper.delete(3);
+		
+		Assert.isTrue(0==coffeeMapper.getAll().size(), "testDelete error !");
+	}
+	
 }
