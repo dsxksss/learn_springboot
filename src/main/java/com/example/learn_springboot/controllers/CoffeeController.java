b9a80312f4b94b8bda236e2e68e2277fc0b20f8d
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-01-02 13:02:08 +0800

    删除mybatis相关依赖 整合spring-boot-jpa

--- src/main/java/com/example/learn_springboot/controllers/CoffeeController.java
+++ src/main/java/com/example/learn_springboot/controllers/CoffeeController.java
@@ -1,7 +1,6 @@
 package com.example.learn_springboot.controllers;
 
 import com.example.learn_springboot.entitys.Coffee;
-import com.example.learn_springboot.mappers.CoffeeMapper;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 /* RESTful API的方式来设计的URL请求
- * 获取所有咖啡信息：GET /coffees
+ *获取所有咖啡信息：GET /coffees
  *获取某款咖啡信息：GET /coffees/{id}
  *新增一款咖啡：POST /coffees
  *修改某款咖啡信息：PUT /coffees/{id}
@@ -26,48 +25,45 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 // 路由根路径
-@RequestMapping("/coffees")
+// @RequestMapping("/coffees")
 public class CoffeeController {
 
   // TODO 待增加post、put、delete方法
-  @Autowired
-  private CoffeeMapper coffeeMapper;
+  
 
   // 获取全部咖啡信息方法
   // 使用URL例子 : http://localhost:2546/coffees/
-  @GetMapping
-  public List<Coffee> getCoffees() {
-    List<Coffee> coffeeList = coffeeMapper.getAll();
-    return coffeeList;
-  }
+  // @GetMapping
+  // public List<Coffee> getCoffees() {
+  // }
 
   // 当URL指向的是某一具体业务资源（或者资源列表），例如博客，用户时，使用@PathVariable
   // 当URL需要对资源或者资源列表进行过滤，筛选时，用@RequestParam
 
   // 通过id获取单个咖啡信息方法
   // 使用URL例子 : http://localhost:2546/coffees/1
-  @GetMapping("/{id}")
+  // @GetMapping("/{id}")
   // @PathVariable 会去搜索path上是否有被{}包裹起来的同名的id值
   //! 并且表示该值作用于下方的参数中 , 需注意同名
-  public Coffee getCoffee(@PathVariable String id) {
-    Coffee coffee = coffeeMapper.getOne(id);
-    // 如果没找到的话 则返回404状态码
-    if (coffee == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-    // 找到则返回
-    return coffee;
-  }
+  // public Coffee getCoffee(@PathVariable String id) {
+    // Coffee coffee = coffeeMapper.getOne(id);
+    // // 如果没找到的话 则返回404状态码
+    // if (coffee == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    // // 找到则返回
+    // return coffee;
+  // }
 
   // TODO 验证客户端数据是否有效且格式有效
   // 添加咖啡信息方法
-  @PostMapping
-  public Coffee addCoffee(@RequestBody Coffee coffee) {
-    Coffee newCoffee = coffee;
-    // 生成ID
-    newCoffee.setId(UUID.randomUUID().toString());
-    // 生成13位时间戳 (13位时间戳是精确到毫秒)
-    // 如果想生成10位时间戳的话 (10位时间戳是精确到秒) 在13位基础上除以1000即可
-    newCoffee.setCreateTime(new Date().getTime() / 1000);
-    coffeeMapper.insert(newCoffee);
-    return newCoffee;
-  }
+  // @PostMapping
+  // public Coffee addCoffee(@RequestBody Coffee coffee) {
+  //   Coffee newCoffee = coffee;
+  //   // 生成ID
+  //   newCoffee.setId(UUID.randomUUID().toString());
+  //   // 生成13位时间戳 (13位时间戳是精确到毫秒)
+  //   // 如果想生成10位时间戳的话 (10位时间戳是精确到秒) 在13位基础上除以1000即可
+  //   newCoffee.setCreateTime(new Date().getTime() / 1000);
+  //   coffeeMapper.insert(newCoffee);
+  //   return newCoffee;
+  // }
 }
