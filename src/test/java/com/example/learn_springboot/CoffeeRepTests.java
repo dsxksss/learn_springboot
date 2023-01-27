9c2d48555e0c0fa099dfbdfd4d7dfcddf5a5f1bd
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-01-27 16:26:40 +0800

    fix：org.springframework.transaction.TransactionSystemException:  Could not commit JPA transaction; nested exception is javax.persistence.RollbackException: Error  while committing the transaction

--- src/test/java/com/example/learn_springboot/CoffeeRepTests.java
+++ src/test/java/com/example/learn_springboot/CoffeeRepTests.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 @SpringBootTest
@@ -73,13 +74,19 @@ public class CoffeeRepTests {
 
   @Test
   @Order(3)
+  /*
+   * 如果一个类或者一个类中的 public 方法上被标注@Transactional 注解的话，Spring 容器就会在启动的时候为其创建一个代理类，
+   * 在调用被@Transactional 注解的 public 方法的时候，实际调用的是，TransactionInterceptor 类中的 invoke()方法。
+   * 这个方法的作用就是在目标方法之前开启事务，方法执行过程中如果遇到异常的时候回滚事务，方法调用完成之后提交事务。
+   */
+  @Transactional
   void testUpdate() throws Exception {
     Coffee oldCoffee = coffeeRep.findById("3").get();
     oldCoffee.setName("test update ! ! !");
     // save()方法在不指定主键的情况下，即实体对象的主键为null时，执行的是insert操作，
     // 而在指定主键的情况下，执行的操作为update操作
 
-    //FIXME 有时候save同一个事务save两次会报 org.springframework.transaction.TransactionSystemException: Could not commit JPA transaction; nested exception is javax.persistence.RollbackException: Error while committing the transaction
+    System.out.println(oldCoffee);
     coffeeRep.save(oldCoffee);
     Coffee newCoffee = coffeeRep.findById("3").get();
     Assert.isTrue(
