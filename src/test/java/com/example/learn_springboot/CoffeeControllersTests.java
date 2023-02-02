63f297b95ec1c85639de55fba71352276214007d
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-02-02 22:00:02 +0800

    去除多余库

--- src/test/java/com/example/learn_springboot/CoffeeControllersTests.java
+++ src/test/java/com/example/learn_springboot/CoffeeControllersTests.java
@@ -12,7 +12,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest(
